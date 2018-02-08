32272c96d4eb5c446239f55b8af869dafd767785
adam <athornton@gmail.com>
2018-02-08 15:04:53 -0700

    Skip missing transformed files with error message.

--- squash_migrator/loader.py
+++ squash_migrator/loader.py
@@ -40,64 +40,67 @@ class Loader(Actuator):
         numfiles = len(inputfiles)
         jobmap = {}
         for fname in inputfiles:
-            with open(fname, "r") as f:
-                self.logger.debug("Loading '%s' for transmission" % fname)
-                job = json.load(f)
-                # _job_number is used in new jobs for correlation but should
-                #   not be sent
-                jobnumstr = os.path.basename(fname)[4:-5]
-                jobnum = int(jobnumstr)
-                save_num = None
-                if "_job_number" in job:
-                    save_num = job["_job_number"]
-                    del job["_job_number"]
-                if save_num is not None and save_num != jobnum:
-                    errstr = ("_job_number %d did not match filename %d" %
-                              (save_num, jobnum))
-                    self.logger.error(errstr)
-                url = self.to_url + "/job"
-                self.logger.info(
-                    "Sending transformed job %d to %s" % (jobnum, url))
-                resp = session.post(url, json=job)
-                if (resp.status_code < 200 or
-                        resp.status_code > 299):
-                    # Should always be 202 if it worked.
-                    self.logger.error("Error posting '%s': HTTP %d / '%s'" %
-                                      (fname, resp.status_code, resp.text))
+            try:
+                with open(fname, "r") as f:
+                    self.logger.debug("Loading '%s' for transmission" % fname)
+                    job = json.load(f)
+                    # _job_number is used in new jobs for correlation but
+                    #   should not be sent
+                    jobnumstr = os.path.basename(fname)[4:-5]
+                    jobnum = int(jobnumstr)
+                    save_num = None
+                    if "_job_number" in job:
+                        save_num = job["_job_number"]
+                        del job["_job_number"]
+                        if save_num is not None and save_num != jobnum:
+                            errstr = ("_job_number %d != filename %d" %
+                                      (save_num, jobnum))
+                            self.logger.error(errstr)
+                    url = self.to_url + "/job"
+                    self.logger.info(
+                        "Sending transformed job %d to %s" % (jobnum, url))
+                    resp = session.post(url, json=job)
+                    if (resp.status_code < 200 or
+                            resp.status_code > 299):
+                        # Should always be 202 if it worked.
+                        self.logger.error("POST error '%s': HTTP %d / '%s'" %
+                                          (fname, resp.status_code, resp.text))
                     continue
-                try:
-                    r_json = resp.json()
-                    message = r_json["message"]
-                    statuslink = r_json["status"]
-                except (json.decoder.JSONDecodeError, KeyError) as exc:
-                    self.logger.error("Malformed response from " +
-                                      "%s (%s): %s" % (url, str(exc),
-                                                       resp.text))
-                    continue
-                # This is cheesy, but we rely on the filename format
-                #  and the message format to just extract the old and new
-                #  job IDs
-                # To wit, 'job-XYZ.json' and
-                #  'Job `XYZ` successfully created' or similar, where XYZ
-                #  is within backticks.
-                try:
-                    new_jobnum = int(message.split('`')[1])
-                    jobmap[jobnum] = new_jobnum
-                except ValueError:
-                    errstr = "Could not determine job numbers for '%s' % fname"
-                    self.logger.error(errstr)
-                try:
-                    self._check_status(statuslink)
-                except (RuntimeError, KeyError,
-                        json.decoder.JSONDecodeError) as exc:
-                    self.logger.error("Data load failed: %s" % str(exc))
-                    continue
-                so_far = so_far + 1
-                self.logger.info("%s: %d/%d" % (fname, so_far, numfiles))
-        if jobmap:
-            with open(os.path.join(self.directory, "jobmap.json"), "w") as f:
-                json.dump(jobmap, f)  # This makes the object key a string
-                # because JSON
+                    try:
+                        r_json = resp.json()
+                        message = r_json["message"]
+                        statuslink = r_json["status"]
+                    except (json.decoder.JSONDecodeError, KeyError) as exc:
+                        self.logger.error("Malformed response from " +
+                                          "%s (%s): %s" % (url, str(exc),
+                                                           resp.text))
+                        continue
+                    # This is cheesy, but we rely on the message format
+                    #  to extract the new job ID
+                    # To wit, 'Job `XYZ` accepted' or similar, where
+                    #  XYZ is within backticks.
+                    try:
+                        new_jobnum = int(message.split('`')[1])
+                        jobmap[jobnum] = new_jobnum
+                    except ValueError:
+                        errstr = "Could not get job numbers for '%s' % fname"
+                        self.logger.error(errstr)
+                    try:
+                        self._check_status(statuslink)
+                    except (RuntimeError, KeyError,
+                            json.decoder.JSONDecodeError) as exc:
+                        self.logger.error("Data load failed: %s" % str(exc))
+                        continue
+                    so_far = so_far + 1
+                    self.logger.info("%s: %d/%d" % (fname, so_far, numfiles))
+                if jobmap:
+                    with open(os.path.join(self.directory, "jobmap.json"),
+                              "w") as f:
+                        # This makes the object key a string
+                        json.dump(jobmap, f)
+                    # because JSON
+            except OSError:
+                self.logger.error("Could not read file '%s'" % fname)
 
     def _check_status(self, statuslink):
         self.logger.info("Checking S3 upload status.")
